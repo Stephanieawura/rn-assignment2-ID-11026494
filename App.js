b7c9ac0a255af2f0ad759b991328ee78091d46bb
Stephanieawura <sadunyo@st.ug.edu.gh>
2024-05-24 16:51:23 +0000

    First commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>"My name is Stephanie Awurabena Dunyo".</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'purple',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
